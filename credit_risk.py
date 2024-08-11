ee3ecd0e6aa5a4fb5ad3a8afaad5e7e461a0dbbf
Namylus <follynamylus@gmail.com>
2024-08-11 13:19:29 +0100

    source

--- credit_risk.py
+++ credit_risk.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pickle as pkl
 
 
-st.title("Loan Default Prediction Application Using Random Forest")
+st.title("Credit Risk Prediction Using Xtreme Gradient Boosting Algorithm (XGBoost)")
 
 
 st.sidebar.title("Input The Feature Values")
@@ -21,6 +21,7 @@ li = st.sidebar.selectbox(label='loan_intent', options=["EDUCATION","MEDICAL","V
 lg = st.sidebar.selectbox(label='loan_grade', options=["A","B","C","D","E","F","G"])
 cbdf = st.sidebar.selectbox(label='cb_person_default_on_file', options=["Y","N"])
 
+
 df = pd.DataFrame()
 df["person_age"] = [pa]
 df['person_income'] = [pi]
@@ -34,26 +35,31 @@ df['person_home_ownership'] = [pho]
 df['loan_intent'] = [li]
 df['loan_grade'] = [lg]
 df['cb_person_default_on_file'] = [cbdf]
+df['Loan_to_Income_Ratio'] = df['loan_amnt'] / df['person_income']
+df['Age_Group'] = pd.cut(df['person_age'], bins=[0, 25, 40, 65, 90], labels=['Young', 'Adult', 'Senior', 'Elderly'])
 
-cats = ["person_home_ownership","loan_intent","loan_grade","cb_person_default_on_file"]
+cats = ["person_home_ownership","loan_intent","loan_grade","cb_person_default_on_file",'Age_Group']
 en_df = pd.get_dummies(df[cats])
 df.drop(cats, axis = 1, inplace=True)
 df = df.join(en_df)
 dt = {"person_age":[False],"person_income":[False],"person_emp_length":[False],"loan_amnt":[False],"loan_int_rate":[False],
         "loan_percent_income":[False],
-        "cb_person_cred_hist_length":[False],"person_home_ownership_MORTGAGE":[False],"person_home_ownership_OTHER":[False],
+        "cb_person_cred_hist_length":[False],"Loan_to_Income_Ratio":[False],
+        "person_home_ownership_MORTGAGE":[False],"person_home_ownership_OTHER":[False],
         "person_home_ownership_OWN":[False],"person_home_ownership_RENT":[False],"loan_intent_DEBTCONSOLIDATION":[False],
         "loan_intent_EDUCATION":[False],
         "loan_intent_HOMEIMPROVEMENT":[False],'loan_intent_MEDICAL':[False],'loan_intent_PERSONAL':[False],"loan_intent_VENTURE":[False],
         "loan_grade_A":[False],'loan_grade_B':[False],'loan_grade_C':[False],'loan_grade_D':[False],'loan_grade_E':[False],'loan_grade_F':[False],
-        "loan_grade_G":[False],'cb_person_default_on_file_N':[False],'cb_person_default_on_file_Y':[False]}
+        "loan_grade_G":[False],'cb_person_default_on_file_N':[False],'cb_person_default_on_file_Y':[False],
+        "Age_Group_Young":[False],"Age_Group_Adult":[False],"Age_Group_Senior":[False],
+        "Age_Group_Elderly":[False]}
 dt_df = pd.DataFrame(dt)
 for i in dt_df.columns :
     if i in df.columns :
         dt_df[i] = df[i]
     else :
         dt_df[i] = dt_df[i]
-with open('model.pkl', 'rb') as f:
+with open('xgboost.pkl', 'rb') as f:
     rf_model = pkl.load(f)
     pred = rf_model.predict(dt_df)
 if pred[0] == 0 :
